ce7228c7b5ffa18a7eb9a59b06f6f0189942f4d4
Guilherme Ribeiro <desaribeiro.guilherme@gmail.com>
2023-06-05 11:57:08 -0300

    feat: add servlet HealthCheck

---
+++ src/main/java/servlet/HealthCheckServlet.java
@@ -0,0 +1,31 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/health-check")
+public class HealthCheckServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+    public HealthCheckServlet() {
+        super();
+    }
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println(LocalDateTime.now() + " debug: call /health-check");
+		
+		PrintWriter out = resp.getWriter();
+		out.println("<b>health check</b>");
+		out.println("<br>");
+		out.println("ok");
+	}
+
+}
